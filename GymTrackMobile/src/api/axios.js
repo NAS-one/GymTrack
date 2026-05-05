82da2874e3cef6ad66e08d1b5d09c0d92e48d921
victor0alexis <131323095+victor0alexis@users.noreply.github.com>
2026-05-04 23:39:22 -0400

    cambio de axios

--- GymTrackMobile/src/api/axios.js
+++ GymTrackMobile/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // LA IP DE TU COMPUTADORA
-const IP_LOCAL = "192.168.1.85";
+const IP_LOCAL = "10.33.12.50";
 
 const instance = axios.create({
   baseURL: `http://${IP_LOCAL}:3000`,
